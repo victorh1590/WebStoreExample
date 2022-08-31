3257af5cf2924bc5c43a6634f616b66362fa2a7c
Victor H. Faria <victorfaria1590@gmail.com>
2022-08-31 12:34:45 -0300

    ViewModel containing the page information and a tag helper to generate the links on the view. Also enabling it in _ViewImports.

---
+++ Store/Infrastructure/PageLinkTagHelper.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Store.Models.ViewModels;
+
+namespace Store.Infrastructure
+{
+  [HtmlTargetElement("div", Attributes = "page-model")]
+  public class PageLinkTagHelper : TagHelper
+  {
+    private IUrlHelperFactory _urlHelperFactory;
+    public PageLinkTagHelper(IUrlHelperFactory urlHelperFactory)
+    {
+      _urlHelperFactory = urlHelperFactory;
+    }
+
+    [ViewContext]
+    [HtmlAttributeNotBound]
+    public ViewContext? ViewContext { get; set; }
+    public PagingInfo? PageModel { get; set; }
+    public string? PageAction { get; set; }
+    public override void Process(TagHelperContext context, TagHelperOutput output) // Add link to div.
+    {
+      if (ViewContext != null && PageModel != null)
+      {
+        IUrlHelper urlHelper = _urlHelperFactory.GetUrlHelper(ViewContext);
+        TagBuilder result = new TagBuilder("div");
+        for (int i = 1; i <= PageModel.TotalPages; i++)
+        {
+          TagBuilder tag = new TagBuilder("a");
+          tag.Attributes["href"] = urlHelper.Action(PageAction, new { productPage = i });
+          tag.InnerHtml.Append(i.ToString());
+          result.InnerHtml.AppendHtml(tag);
+        }
+        output.Content.AppendHtml(result.InnerHtml);
+      }
+    }
+  }
+}
