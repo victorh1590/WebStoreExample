8d533a64279271a6ed6fec8002868e5f2845d748
Victor H. Faria <victorfaria1590@gmail.com>
2022-08-31 12:12:17 -0300

    Pagination and test.

--- Store.Tests/HomeControllerTests.cs
+++ Store.Tests/HomeControllerTests.cs
@@ -11,23 +11,52 @@ namespace Store.Tests;
 
 public class HomeControllerTests
 {
-    [Fact]
-    public void Can_Use_Repository()
-    {
-        Mock<IStoreRepository> mock = new Mock<IStoreRepository>();
-        mock.Setup(m => m.Products).Returns((new Product[] {
+  [Fact]
+  public void Can_Use_Repository()
+  {
+    // Arrange
+    Mock<IStoreRepository> mock = new Mock<IStoreRepository>();
+    mock.Setup(m => m.Products).Returns((new Product[] {
             new Product {ProductID = 1, Name = "P1"},
             new Product {ProductID = 2, Name = "P2"}
         }).AsQueryable<Product>());
 
-        HomeController controller = new HomeController(mock.Object);
+    HomeController controller = new HomeController(mock.Object);
 
-        IEnumerable<Product>? result = (controller.Index() as ViewResult)?.ViewData.Model as IEnumerable<Product>;
+    // Act
+    IEnumerable<Product>? result = (controller.Index() as ViewResult)?.ViewData.Model as IEnumerable<Product>;
 
-        Product[] prodArray = result?.ToArray() ?? Array.Empty<Product>();
+    // Assert
+    Product[] prodArray = result?.ToArray() ?? Array.Empty<Product>();
+    Assert.True(prodArray.Length == 2);
+    Assert.Equal("P1", prodArray[0].Name);
+    Assert.Equal("P2", prodArray[1].Name);
+  }
 
-        Assert.True(prodArray.Length == 2);
-        Assert.Equal("P1", prodArray[0].Name);
-        Assert.Equal("P2", prodArray[1].Name);
-    }
+  [Fact]
+  public void Can_Paginate()
+  {
+    // Arrange
+    Mock<IStoreRepository> mock = new Mock<IStoreRepository>();
+    mock.Setup(m => m.Products).Returns((new Product[] {
+            new Product {ProductID = 1, Name = "P1"},
+            new Product {ProductID = 2, Name = "P2"},
+            new Product {ProductID = 3, Name = "P3"},
+            new Product {ProductID = 4, Name = "P4"},
+            new Product {ProductID = 5, Name = "P5"}
+        }).AsQueryable<Product>());
+
+    HomeController controller = new HomeController(mock.Object);
+    controller.PageSize = 3;
+
+    // Act
+    IEnumerable<Product> result =
+    (controller.Index(2) as ViewResult)?.ViewData.Model as IEnumerable<Product> ?? Enumerable.Empty<Product>();
+
+    // Assert
+    Product[] prodArray = result.ToArray();
+    Assert.True(prodArray.Length == 2);
+    Assert.Equal("P4", prodArray[0].Name);
+    Assert.Equal("P5", prodArray[1].Name);
+  }
 }
--- Store/Controllers/HomeController.cs
+++ Store/Controllers/HomeController.cs
@@ -6,12 +6,19 @@ namespace Store.Controllers
   public class HomeController : Controller
   {
     private IStoreRepository _repository;
+    public int PageSize = 4;
 
     public HomeController(IStoreRepository repository) 
     {
       _repository = repository;
     }
 
-    public IActionResult Index() => View(_repository.Products); // Send products from repository to view.
+    // public IActionResult Index() => View(_repository.Products); // Send products from repository to view.
+
+    public ViewResult Index(int productPage = 1) 
+      => View(_repository.Products
+      .OrderBy(p => p.ProductID)
+      .Skip((productPage - 1) * PageSize)
+      .Take(PageSize));
   }
 }
