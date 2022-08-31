a8c6b98ea1b966f503a9efebaaf5a849b68dc77f
Victor H. Faria <victorfaria1590@gmail.com>
2022-08-31 18:22:36 -0300

    Make page counter consider the category.

--- Store/Controllers/HomeController.cs
+++ Store/Controllers/HomeController.cs
@@ -34,7 +34,8 @@ namespace Store.Controllers
       {
         CurrentPage = productPage,
         ItemsPerPage = PageSize,
-        TotalItems = _repository.Products.Count()
+        TotalItems = (category == null ? 
+          _repository.Products.Count() : _repository.Products.Where(e => e.Category == category).Count())
       },
       CurrentCategory = category
     });
