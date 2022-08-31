aa2aa78e471030e937cb3021dba84ecdc16ab18a
Victor H. Faria <victorfaria1590@gmail.com>
2022-08-31 15:56:31 -0300

    Bootstrap added to project. Initial page styling.

--- Store/Infrastructure/PageLinkTagHelper.cs
+++ Store/Infrastructure/PageLinkTagHelper.cs
@@ -21,6 +21,11 @@ namespace Store.Infrastructure
     public ViewContext? ViewContext { get; set; }
     public PagingInfo? PageModel { get; set; }
     public string? PageAction { get; set; }
+    public bool PageClassesEnabled { get; set; } = false;
+    public string PageClass { get; set; } = String.Empty;
+    public string PageClassNormal { get; set; } = String.Empty;
+    public string PageClassSelected { get; set; } = String.Empty;
+
     public override void Process(TagHelperContext context, TagHelperOutput output) // Add link to div.
     {
       if (ViewContext != null && PageModel != null)
@@ -31,6 +36,11 @@ namespace Store.Infrastructure
         {
           TagBuilder tag = new TagBuilder("a");
           tag.Attributes["href"] = urlHelper.Action(PageAction, new { productPage = i });
+          if (PageClassesEnabled)
+          {
+            tag.AddCssClass(PageClass);
+            tag.AddCssClass(i == PageModel.CurrentPage ? PageClassSelected : PageClassNormal);
+          }
           tag.InnerHtml.Append(i.ToString());
           result.InnerHtml.AppendHtml(tag);
         }
